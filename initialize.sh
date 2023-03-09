cf467130fe618c071d23c1915147c0b5b12c94f1
Jorge Lopez <alelo.lopez07@gmail.com>
2023-03-09 12:36:15 -0600

    [chore] comments

--- initialize.sh
+++ initialize.sh
@@ -2,8 +2,8 @@
 # initialize.sh script file
 #--------------------------------------------------------------------------
 #
-# After executing the genesis script, this script will help you initialize the containers
-# for parking lot project.
+# After executing the genesis script or you are in a genesis, this script will 
+# help you initialize the containers for parking lot project.
 
 docker-compose build --no-cache
 docker-compose up -d
